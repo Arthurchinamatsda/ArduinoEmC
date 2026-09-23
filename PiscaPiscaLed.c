ec8f17a4e62b99047a59f3ee04d619bd418046d1
Axtwal <ademarteiyuchina@gmail.com>
2026-09-22 21:15:05 -0300

    Pisca Pisca LED

---
+++ PiscaPiscaLed.c
@@ -0,0 +1,14 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void){
+	DDRD |= (1<<PD7);
+
+	while(1){
+		PORTD |= (1<<PD7);
+		_delay_ms(1000);
+
+		PORTD &= ~(1<<PD7);
+		_delay_ms(1000);
+	}
+}
